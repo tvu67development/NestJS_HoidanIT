229566914a0cbc96c82bcabd79d5f0680090e5b8
tvu67 <vuquangtrungdhsp@gmail.com>
2023-07-29 11:09:49 +0700

    29/07

---
+++ src/controllers/homeController.js
@@ -0,0 +1,66 @@
+const connection = require('../config/database');
+const { getAllUsers } = require('../services/CRUDService');
+
+// su dung asyn - await cho cac ham nao call DB
+const getHomepage = async (req, res) => {
+    let results = await getAllUsers();
+    return res.render('home.ejs', { listUsers: results });
+}
+
+const getABC = (req, res) => {
+    res.send("check ABC");
+}
+
+const getHoiDanIT = (req, res) => {
+    res.render('sample.ejs');
+}
+
+const postCreateUser = async (req, res) => {
+    // nay la noi chuoi trong JS, ko can dung dau "+", chi can dung dau ","
+    console.log("req.body: ", req.body);
+    // let email = req.body.email;
+    // let name = req.body.myname;
+    // let city = req.body.city;
+
+    let { email, name, city } = req.body;
+    // connection.query(
+    //     // Dung dau '' thi khi xuong dong se bao loi cau, dung dau `` thi ko bi vay
+    //     `INSERT INTO Users (email, name, city) 
+    //     VALUES (?, ?, ?)`,
+    //     [email, myname, city],
+    //     function (err, results) {
+    //         res.send(' Created user succeed !')
+    //     }
+    // );
+
+    let [results, fields] = await connection.query(
+        `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`, [email, name, city]
+    );
+
+    console.log("results: ", results);
+
+    res.send('Created user succeed !');
+
+    // connection.query(
+    //     'select * from Users u', 
+    //     function (err, results, fields) {
+    //         console.log("results", results);
+    //     }
+    // )
+
+    // const [results, fields] = await connection.query('select * from Users u');
+    // console.log("results", results);
+    // res.send('create a new user');
+}
+
+const getCreatePage = (req, res) => {
+    res.render("create.ejs")
+}
+
+module.exports = {
+    getHomepage,
+    getABC,
+    getHoiDanIT,
+    postCreateUser,
+    getCreatePage
+}
---
+++ src/routes/web.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { getHomepage, getABC, getHoiDanIT, postCreateUser, getCreatePage } = require('../controllers/homeController');
+const router = express.Router();
+
+// quy chuan su dung:         router.Method('/router', handler);
+// neu sua doi giao dien, thi bat dau check trong phan routes nay 
+router.get("/", getHomepage);
+
+router.get("/abc", getABC);
+
+router.get("/hoidanit", getHoiDanIT);
+
+router.get("/create", getCreatePage);
+
+router.post("/create-user", postCreateUser);
+
+module.exports = router;  // export default
+
