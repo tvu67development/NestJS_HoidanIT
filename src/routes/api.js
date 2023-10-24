f7f9bc271e6fdcb8e196f48b671aeeeeaf1143d7
tvu67 <vuquangtrungdhsp@gmail.com>
2023-10-24 16:22:46 +0700

    24-10-2023 (completed CRUD)

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const routerAPI = express.Router();
-const { getUsersAPI } = require("../controllers/apiController")
-
-// quy chuan su dung:         router.Method('/router', handler);
-// neu sua doi giao dien, thi bat dau check trong phan routes nay 
-routerAPI.get("/", (req, res) => {
-    res.send("Hello APIs")
-});
-
-routerAPI.get("/abc", (req, res) => {
-    res.status(200).json({
-        data: "Hello API2 abc"
-    })
-});
+const { getUsersAPI, postCreateUserAPI, putUpdateUserAPI, deleteUserAPI } = require("../controllers/apiController")
 
 routerAPI.get("/users", getUsersAPI);
+
+routerAPI.post("/users", postCreateUserAPI);
+
+routerAPI.put("/users", putUpdateUserAPI);
+
+routerAPI.delete("/users", deleteUserAPI);
 
 module.exports = routerAPI;  // export default
 
